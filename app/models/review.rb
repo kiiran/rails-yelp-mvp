2e2476b71b83e0105ad590562e5d8d40730d6b77
Ciaran Edwards <ciaran.edwards@live.com>
2017-02-15 12:47:36 +0000

    Starting to add the styles. Fixed the seeds.db file.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,4 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0,1,2,3,4,5] }
-  # content cannot be blank (FAILED - 10)
-  # rating cannot be blank (FAILED - 11)
-  # parent restaurant cannot be nil
-  # rating should be an integer (FAILED - 12)
-  # rating should be a number between 0 and 5 (FAILED - 13)
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:        "+351 12 51 32 89",
+    category: 'italian'
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:        "+151 12 51 32 89",
+    category: 'chinese'
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:        "+351 12 51 32 89",
+    category: 'belgian'
+  },
+  {
+    name:         "Chez Steve",
+    address:      "Steve's house, in the middle of his street",
+    phone_number:        "+551 12 51 32 89",
+    category: 'japanese'
+  },
+  {
+    name:         "Une Glace au Navet",
+    address:      "route de la rue 75016 Paris",
+    phone_number:        "+351 12 51 32 89",
+    category: 'french'
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
